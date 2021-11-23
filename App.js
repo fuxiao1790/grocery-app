8dbec12afaddcf8bea4d47a00d9617b1e46344cd
Ken <xiao.fu1790@gmail.com>
2021-11-23 10:28:38 -0500

    routing

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
 import React from 'react';
-
-import MainScreen from './main-screen/main-screen';
+import ItemList from './views/item-list'
+import OrderList from './views/order-list'
+import PreviewOrder from './views/preview-order'
+import StoreList from './views/store-list'
+import {
+  Scene,
+  Router,
+  Stack,
+} from 'react-native-router-flux';
 
 export default class App extends React.Component {
   render() {
-    return <MainScreen />
+    return (
+      <Router>
+        <Stack key="root">
+          <Scene key="login" component={ItemList} hideNavBar/>
+          <Scene key="register" component={OrderList} hideNavBar/>
+          <Scene key="home" component={PreviewOrder} hideNavBar/>
+          <Scene key="home" component={StoreList} hideNavBar/>
+        </Stack>
+      </Router>
+    )
   }
 }
+
+// @react-native-community/masked-view @react-navigation/native @react-navigation/stack react-native-gesture-handler react-native-reanimated react-native-safe-area-context react-native-screens
---
+++ views/preview-order.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default class PreviewOrder extends React.Component {
+
+	render() {
+		return (
+			<View>
+				<Text>
+					Item List
+				</Text>
+			</View>
+		)
+	}
+}
